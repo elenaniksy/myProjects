e99f2b0332a25b931bffba5f272809f1e383c44c
elenaniksy <elfhelen@gmail.com>
2020-02-03 12:12:59 +0300

    Full HTML layout, halt css layout, js script for carousel.

---
+++ MeetUp_Contur/script.js
@@ -0,0 +1,31 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(n) {
+    var i;
+    let slides = document.getElementsByClassName("slideItem");
+    let dots = document.getElementsByClassName("dot");
+
+    if(n < 1) {
+        slideIndex = slides.length;
+    } else if (n > slides.length) {
+        slideIndex = 1;
+    }
+    for(let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className+= "active";
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlides(n) {
+    showSlides(slideIndex = n);
+}
+
